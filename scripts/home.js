e058344d2b4206dd335c8048f892dfea1a4309d9
Yonesh Thapa <hsenoy2022@gmail.com>
2023-04-28 16:02:20 -0400

    added navigation with javascript and edited homepa

---
+++ scripts/home.js
@@ -0,0 +1,8 @@
+
+    //document.write("Am I on the page?");
+    //document.body.innerHTML += "Right way?";
+    //content.innerHTML += "Another direct way";
+
+    //SAFE AND MODERN
+    const target = document.getElementById("content");
+    target.innerHTML += "Safe and modern.";
---
+++ scripts/nav.js
@@ -0,0 +1,13 @@
+document.body.innerHTML = `
+<nav>
+<a href="index.html">
+    Home
+</a>
+<a href="celsius.html">
+    Celsius Calculator
+</a>
+<a href="farenheit.html">
+    Farenheit Calculator
+</a>
+</nav>
+` + document.body.innerHTML;
